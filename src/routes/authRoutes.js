5c697749b963562f5ac88fca38cc1415d013f199
dev-paulo07 <brawlzinho190@gmail.com>
2026-07-24 12:28:23 -0300

    Remove unnecessary line breaks in authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Define que quando houver um POST para /auth/login, chama a função de login do controller
 router.post('/login', authController.login);
 
 module.exports = router;
